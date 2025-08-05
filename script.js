a72230d003619012edfdb17314dfaae418e4d7bb
vitalicc <38910823+vitalicc@users.noreply.github.com>
2025-08-05 21:08:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const map = L.map('map').setView([49.0836, 33.4263], 17);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
+  attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
 const lights = [
